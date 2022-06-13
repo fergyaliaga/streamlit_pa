5c93c13d952840f26ab36b01eb55d3dfb8ea1d3e
fergyaliaga <106988662+fergyaliaga@users.noreply.github.com>
2022-06-13 11:55:49 -0500

    Update memo.py

--- memo.py
+++ memo.py
@@ -4,8 +4,8 @@ import numpy as np
 import urllib.request
 
 @st.experimental_memo
-  def download_data:
-    url="http://server01.labs.org.pe:2005/datos_horarios_contaminacion_lima.csv"
-    filename="datos_horarios.csv"
-    urllib.request.urlretrieve(url,filename)
+def download_data:
+   url="http://server01.labs.org.pe:2005/datos_horarios_contaminacion_lima.csv"
+   filename="datos_horarios.csv"
+   urllib.request.urlretrieve(url,filename)
 download_data()
