8cf6e66df062453e9598dd442b850eed0c0bc049
JosmerCC <106988741+JosmerCC@users.noreply.github.com>
2022-06-18 12:30:21 -0500

    Update grafica2_distrito.py

--- grafica2_distrito.py
+++ grafica2_distrito.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 import urllib.request
+import base64
 #import matplotlib.pyplot as mplt
 #import altair as alt
 #import base64
@@ -17,6 +19,9 @@ st.markdown("""
 	* **Base de datos:** [Servicio Nacional de Meteorología e Hidrología del Perú - SENAMHI] (https://www.datosabiertos.gob.pe/dataset/datos-horarios-de-contanimantes-del-aire-en-lima-metropolitana-servicio-nacional-de).
 	""")
 
+from PIL import Image
+image = Image.open('contaminacion.jpeg')
+st.image(image, caption='La contaminación por el parque automotor antiguo es un problema en Lima Metropolitana', use_column_width=True)
 #from PIL import Image
 #image = Image.open('contaminacion.jpeg')
 #st.image(image, caption='La contaminación por el parque automotor antiguo es un problema en Lima Metropolitana', use_column_width=True)
@@ -76,16 +81,16 @@ st.write('Dimensiones: ' + str(data.shape[0]) + ' filas y ' + str(data.shape[1])
 st.dataframe(data)
 
 #st.text(str(selected_contaminant))
-'''
+
 def filedownload(df):
 	csv = df.to_csv(index=False)
 	b64 = base64.b64encode(csv.encode()).decode()
 	href = f'<a href="data:file/csv;base64,{b64}">Descargar archivo CSV</a> (botón derecho y guardar como ".csv")'
 	return href
 
-st.markdown("<svg><clipPath></clipPath></svg>", unsafe_allow_html=True)
 st.markdown(filedownload(df_selected), unsafe_allow_html=True)
-'''
+
+
 if st.sidebar.button("CSV completo"):
 	st.subheader('Dataset completo')
 	df=pd.read_csv('datos_horarios_contaminacion_lima.csv')
@@ -95,10 +100,49 @@ if st.sidebar.button("CSV completo"):
 if st.button('Gráfico interactivo'):
 	st.header('Gráfico de líneas')
 	datos=df_selected.groupby(['MES']).agg({"PM 10": 'mean', "PM 2.5": 'mean', "SO2": 'mean', "NO2": 'mean', "O3": 'mean', "CO": 'mean'})
-	st.line_chart(datos)
-	#line_chart = alt.Chart(datos, title='probando ando').mark_line().encode(x='MES', y='NO2')
-	#st.altair_chart(line_chart, use_container_width=True)
+	#datos.reset_index(inplace=True)
+	#c=alt.Chart(datos, title='DISTRITO:'+" "+' '.join(selected_district)).mark_line().encode(x='MES', y='ppm:Q')
+	data = datos.reset_index().melt('MES')
+	data.rename(columns={'variable':'contaminante', 'value':'ppm'}, inplace=True)
+	c=alt.Chart(data, title='DISTRITO:'+" "+' '.join(selected_district)).mark_line().encode(
+		x='MES',
+		y='ppm',
+    color='contaminante')
+	st.altair_chart(c, use_container_width=True)
 
+	st.dataframe(data)
+
+	contaminantes=['PM 10', 'PM 2.5', 'SO2', 'NO2', 'O3', 'CO']
+	def getBaseChart():
+		#base=alt.Chart(data).mark_line().encode(x='MES',y='ppm',color='contaminante').properties(width=500, height=400)
+		base = (alt.Chart(data).encode(x=alt.X("MES:T",axis=alt.Axis(title="Mes")),y=alt.Y("ppm:Q", axis=alt.Axis(title="Concentración (ppm)")),).properties(width=500, height=400))
+		return base
+
+	def getSelection():
+		cont_select = alt.selection_multi(fields=["contaminante"], name="Contaminante")
+		cont_color_condition = alt.condition(cont_select, alt.Color("contaminante:N", legend=None), alt.value("lightgrey"))
+		return cont_select, cont_color_condition
+
+	def createChart():
+		cont_select, cont_color_condition = getSelection()
+		make_selector = (alt.Chart(data).mark_circle(size=200).encode(y=alt.Y("contaminante:N", axis=alt.Axis(title="Elija contaminante", titleFontSize=15)),color=cont_color_condition,).add_selection(cont_select))
+		base = getBaseChart()
+		highlight_cont = (base.mark_line(strokeWidth=2).add_selection(cont_select).encode(color=cont_color_condition)).properties(title='DISTRITO:'+" "+' '.join(selected_district))
+		return base, make_selector, highlight_cont, cont_select
+
+	def createTooltip(base, cont_select):
+		nearest = alt.selection(type="single", nearest=True, on="mouseover", fields=["MES"], empty="none")
+		selectors = (alt.Chart(data).mark_point().encode(x="MES:T",opacity=alt.value(0)).add_selection(nearest))
+		points = base.mark_point(size=5, dy=-10).encode(opacity=alt.condition(nearest, alt.value(1), alt.value(0))).transform_filter(cont_select)
+
+		tooltip_text = base.mark_text(align="left",dx=-60,dy=-15,fontSize=15,fontWeight="bold",lineBreak = "\n",).encode(text=alt.condition(nearest, alt.Text("ppm:Q", format=".2f"), alt.value(" "),),).transform_filter(cont_select)
+		rules = (alt.Chart(data).mark_rule(color="white", strokeWidth=1).encode(x="MES:T",).transform_filter(nearest))
+		return selectors, rules, points, tooltip_text
+
+	base, make_selector, highlight_cont, cont_select  = createChart()
+	selectors, rules, points, tooltip_text  = createTooltip(base, cont_select)
+
+	make_selector | alt.layer(highlight_cont, selectors, points,rules, tooltip_text)	
 anios = st.button('Gráfico entre años')
 
 minYear = st.selectbox('Desde', list(range(2010,2021)))
@@ -162,6 +206,24 @@ st.dataframe(cont_distrito)
 
 st.subheader("Gráfica interactiva - ", selec_ditrit )
 st.line_chart(cont_distrito)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
